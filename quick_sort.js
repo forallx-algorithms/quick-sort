404fcba91cac1c0991f209daab3858ef48d5c4b5
Evgeniy Kuznetsov <chamscontacts@gmail.com>
2015-02-08 14:49:52 +0300

    Quick sort template

---
+++ quick_sort.js
@@ -0,0 +1,13 @@
+/*
+ Quick sort algorithm sorts an array for O(n*log(n)) time.
+
+ @author Evgeniy Kuznetsov
+ @date 8.2.2015 14:46
+ */
+
+// @param {Array.<Integer>} a
+// @param {Function} choosePivotFn Optional function for choosing pivot point
+// @return {Array.<Integer>} Sorted version of a given array
+function quickSort(a){
+
+}
